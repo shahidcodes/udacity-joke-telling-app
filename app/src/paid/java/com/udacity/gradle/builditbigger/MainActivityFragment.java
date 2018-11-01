69e918dd367c59f5b4010a8dabc435337b3adc31
Shahid Kamal <17620737+shahidcodes@users.noreply.github.com>
2018-11-01 09:57:31 +0530

    review fixes

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,14 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-import ml.shahidkamal.libjoker.Joker;
 
 
 /**
